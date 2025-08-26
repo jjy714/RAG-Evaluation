613889951ba7c0761f0bf885a2b39574be8783f7
Jason <jjy714@gmail.com>
2025-08-26 09:17:02 +0900

    .
    Signed-off-by: Jason <jjy714@gmail.com>

--- DB/start_server.sh
+++ DB/start_server.sh
@@ -6,3 +6,24 @@ brew services start mongodb-community
 uvicorn app.main:app --reload
 
 #Swagger UI: http://127.0.0.1:8000/docs
+
+docker run \
+    -p 9017:27017 \
+    --rm \
+    --network mongo-net \
+    --name mongo \
+    -e MONGO_INITDB_ROOT_USERNAME=root \
+    -e MONGO_INITDB_ROOT_PASSWORD=changeme \
+    -v mongo-data:/data/db \
+    -d mongo
+
+docker run \
+    --name mongo-express \
+    --network mongo-net \
+    -p 9018:8081 \
+    --rm \
+    -e ME_CONFIG_MONGODB_ADMINUSERNAME=root \
+    -e ME_CONFIG_MONGODB_ADMINPASSWORD=changeme \
+    -e ME_CONFIG_MONGODB_SERVER=mongo \
+    -d mongo
+
