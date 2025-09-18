00b758825b5937cf35bc51943375dd12c6a14092
Minji Choi <cmj04250@gmail.com>
2025-09-18 11:25:10 +0900

    add locust_settiong & post data

--- RAG_Evaluation/main.py
+++ RAG_Evaluation/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from api.v1.endpoints.evaluator import evaluator
 from api.v1.routers import api_router
 from graphs.main_graph import create_main_graph, EvaluationState
+from core.post_data import DataPointApiClient
+from typing import Any, Dict
 
 app = FastAPI(
     title="RAG Evaluation API",
@@ -25,3 +27,14 @@ Log as a file and store in User's -> session -> table in DB
 def read_root():
     return {"status": "ok"}
 
+###### 
+@app.post("/send-datapoint")
+async def main(session_id: str, endpoint: str, payload: Dict[str, Any]):
+    sender = DataPointApiClient(session_id, endpoint)
+    return await sender.send_datapoint(payload)
+
+
+@app.post("/eval_result") # 
+async def get_eval_result(payload: Dict[str, Any]):
+    print("payload:", payload)
+    return {"eval_result": payload}
